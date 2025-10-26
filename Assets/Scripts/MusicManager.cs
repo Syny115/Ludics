926035340bad34bada9b3d911c5aa8ef6fb4d4c0
Hyfa <nilmuniente@gmail.com>
2025-10-26 11:40:07 +0100

    Version 0.1.1
    He cambiado los números de las operaciones y he hecho que se cambie la música cuando empiezas a jugar, pero no he sabido hacerlo al revés.
    
    Probad ya de manera gratuita el juego que revolucionará la industria educativa!, por la mano de los Lúdics que os traen...
    
    TICTOCAT

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip menuMusic;
+    public AudioClip gameModeMusic;
+
+    void Start()
+    {
+        // Reproduce la música del menú al iniciar
+        PlayMenuMusic();
+    }
+
+    public void PlayMenuMusic()
+    {
+        audioSource.Stop();
+        audioSource.clip = menuMusic;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+
+    public void PlayGameModeMusic()
+    {
+        audioSource.Stop();
+        audioSource.clip = gameModeMusic;
+        audioSource.loop = true;
+        audioSource.Play();
+ 
+    }
+}
+
